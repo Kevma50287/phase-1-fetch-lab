9050eff1a6435281cb48d63941fdbb6ae297de2f
Kevin <magavakevin10@gmail.com>
2022-07-20 12:53:02 -0400

    commit tree 11b66859efef53f0bf143405227cf3e6a8d8baf0

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
@@ -15,3 +17,28 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+//fetch object containing 10 books and their corresponding indices
+
+/*
+const fetchpractice = fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json)
+
+//create an array of book names, find 5th book
+const createArray = (object, keyname) => {
+  let newArray = []
+  for (const key in object) {
+    if (Object.hasOwnProperty(keyname)) {
+      newArray.push(object[keyname]);
+    };
+  }
+  return newArray
+}
+*/
+
+
+
+//create an array of characters, for 1031 character
+
+//create var that will add the pages of each book (element in object) 
